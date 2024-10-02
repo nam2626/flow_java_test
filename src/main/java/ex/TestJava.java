8ecff471b57d1323694030a3a66101f0ed1a9063
nam2626 <nam2626@naver.com>
2024-10-02 16:31:58 +0900

    두번째 커밋

--- src/main/java/ex/TestJava.java
+++ src/main/java/ex/TestJava.java
@@ -1,5 +1,8 @@
+package ex;
+
 public class TestJava {
     public static void main(String[] args) {
+        System.out.println("Hello, World!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World!");
     }
